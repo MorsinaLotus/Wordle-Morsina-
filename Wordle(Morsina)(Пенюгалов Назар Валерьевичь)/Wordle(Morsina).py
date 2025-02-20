9c81edf627f3b03a4408c5174989d39f44b1e1c8
Morsina <morsinalotus@gmail.com>
2025-02-20 22:57:37 +1000

    Wordle(Morsina).py

--- Wordle(Morsina)(Пенюгалов Назар Валерьевичь)/Wordle(Morsina).py
+++ Wordle(Morsina)(Пенюгалов Назар Валерьевичь)/Wordle(Morsina).py
@@ -5,7 +5,7 @@ from words import *
 
 pygame.init()
 
-# Константы
+
 
 WIDTH, HEIGHT = 633, 900
 
@@ -38,26 +38,25 @@ LETTER_X_SPACING = 85
 LETTER_Y_SPACING = 12
 LETTER_SIZE = 75
 
-# Глобальные переменные
+
 
 guesses_count = 0
 
-# это двумерный список, в котором будут храниться догадки. Догадка будет представлять собой список букв.
-# Список будет пролистан, и каждая буква в каждом будет выведена на экран.
+
 guesses = [[]] * 6
 
 current_guess = []
 current_guess_string = ""
 current_letter_bg_x = 110
 
-# Индикаторы
+
 indicators = []
 
 game_result = ""
 
 class Letter:
     def __init__(self, text, bg_position):
-        # Инициализирует все переменные, включая текст, цвет, положение, размер 
+       
         self.bg_color = "white"
         self.text_color = "black"
         self.bg_position = bg_position
@@ -70,7 +69,7 @@ class Letter:
         self.text_rect = self.text_surface.get_rect(center=self.text_position)
 
     def draw(self):
-        # Размещает буквы и текст размещается на экране в нужных местах.
+        
         pygame.draw.rect(SCREEN, self.bg_color, self.bg_rect)
         if self.bg_color == "white":
             pygame.draw.rect(SCREEN, FILLED_OUTLINE, self.bg_rect, 3)
@@ -79,14 +78,14 @@ class Letter:
         pygame.display.update()
 
     def delete(self):
-        # Заполняет вместо буквы квадратом освобождая его.
+       
         pygame.draw.rect(SCREEN, "white", self.bg_rect)
         pygame.draw.rect(SCREEN, OUTLINE, self.bg_rect, 3)
         pygame.display.update()
 
 class Indicator:
     def __init__(self, x, y, letter):
-        # Инициализирует такие переменные, как цвет, размер, положение и буква.
+       
         self.x = x
         self.y = y
         self.text = letter
@@ -94,14 +93,14 @@ class Indicator:
         self.bg_color = OUTLINE
 
     def draw(self):
-        # Помещает индикатор и его текст на экране в нужное положение.
+        
         pygame.draw.rect(SCREEN, self.bg_color, self.rect)
         self.text_surface = AVAILABLE_LETTER_FONT.render(self.text, True, "white")
         self.text_rect = self.text_surface.get_rect(center=(self.x+27, self.y+30))
         SCREEN.blit(self.text_surface, self.text_rect)
         pygame.display.update()
 
-# создание индикаторов на экране.
+
 
 indicator_x, indicator_y = 20, 600
 
@@ -163,7 +162,7 @@ def check_guess(guess_to_check):
         game_result = "L"
 
 def play_again():
-    # Выводит на экран текст "Воспроизвести еще раз".
+  
     pygame.draw.rect(SCREEN, "white", (10, 600, 1000, 600))
     play_again_font = pygame.font.Font("assets/SansBold.otf", 40)
     play_again_text = play_again_font.render("Press ENTER to Play Again!", True, "black")
@@ -175,7 +174,7 @@ def play_again():
     pygame.display.update()
 
 def reset():
-    # Resets all global variables to their default states.
+
     global guesses_count, CORRECT_WORD, guesses, current_guess, current_guess_string, game_result
     SCREEN.fill("white")
     SCREEN.blit(BACKGROUND, BACKGROUND_RECT)
@@ -191,7 +190,7 @@ def reset():
         indicator.draw()
 
 def create_new_letter():
-    # Создает новую букву и добавляет ее к предположению.
+    
     global current_guess_string, current_letter_bg_x
     current_guess_string += key_pressed
     new_letter = Letter(key_pressed, (current_letter_bg_x, guesses_count*100+LETTER_Y_SPACING))
@@ -203,7 +202,7 @@ def create_new_letter():
             letter.draw()
 
 def delete_letter():
-    # Удаляет последнюю букву из предложения.
+  
     global current_guess_string, current_letter_bg_x
     guesses[guesses_count][-1].delete()
     guesses[guesses_count].pop()
